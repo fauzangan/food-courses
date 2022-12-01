81b84782aae5c25b52db78137f1a3e1bc27ca9c4
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2022-12-02 00:06:25 +0700

    update text

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <p class="hidden text-gray-500 md:mt-4 md:block">
         Pembelajaran online untuk mahasiswa yang tidak bisa memasak di-kost, untuk itu kami hadir untuk membantu mahasiswa
-        dalam memenuhi kebutuhan gizi mahasiswa sehari-hari, demi menjaga kesehatan mahasiswa agar tidak sakit, lalu ipk jelek,
+        dalam memenuhi kebutuhan gizi mahasiswa sehari-hari, demi menjaga kesehatan mahasiswa agar tidak sakit, lalu IPK jelek,
         depresi dan bunuh diri.
       </p>
 
@@ -30,8 +30,8 @@ export default function Home() {
   </div>
 
   <img
-    alt="Violin"
-    src="https://images.unsplash.com/photo-1484959014842-cd1d967a39cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+    alt="Food"
+    src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
     class="h-full w-full object-cover sm:h-[calc(100%_-_2rem)] sm:self-end sm:rounded-tl-[30px] md:h-[calc(100%_-_4rem)] md:rounded-tl-[60px]"
   />
 </section>
